206be5247baaf29754cc6a5888489b936f6b0ed7
luansiks <luansikssantos10@gmail.com>
2023-09-09 11:39:07 -0300

    up

---
+++ script.js
@@ -0,0 +1,26 @@
+window.alert('AVISO: esse site foi criado por luansiks e 90% das informações presentes nesse site foram tiradas do google.')
+function acontece(){
+    var aleatorio = Math.floor(Math.random() * (10 - 1 + 1)) + 1
+    if(aleatorio == 1){
+        window.alert('CURIOSIDADE: Temos cinco planetas considerados anões.')
+    }else if(aleatorio == 2){
+        window.alert('CURIOSIDADE: Vênus não apresenta lua e se movimenta de forma retrograda.')
+    }else if(aleatorio == 3){
+        window.alert('CURIOSIDADE: Urano é o planeta mais frio.')
+    }else if(aleatorio == 4){
+        window.alert('CURIOSIDADE: Plutão não é planeta.')
+    }else if(aleatorio == 5){
+        window.alert('CURIOSIDADE: O sistema solar possui mais de 200 luas.')
+    }else if(aleatorio == 6){
+        window.alert('CURIOSIDADE: Existem rochas de Marte na Terra')
+    }else if(aleatorio == 7){
+        window.alert('CURIOSIDADE: O planeta mais quente não é o mais próximo do Sol e sim vênus')
+    }else if(aleatorio == 8){
+        window.alert('CURIOSIDADE: O Sol corresponde a 99,86% da massa total do Sistema Solar.')
+    }else if(aleatorio == 9){
+        window.alert('CURIOSIDADE: Mercúrio foi nomeado em homenagem ao mensageiro dos deuses romanos, por causa de sua rotação rápida em torno do sol.')
+    }else{
+        window.alert('CURIOSIDADE: A maioria dos elementos encontrados em outros planetas são hélio e hidrogênio.')
+    }
+}
+
